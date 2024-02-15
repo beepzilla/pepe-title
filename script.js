8dafb23f1ff514061fb4cf1568c111634753a08f
Josh <159769823+beepzilla@users.noreply.github.com>
2024-02-15 13:19:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ var button = document.querySelector('.button');
 function doSetTimeout(i) {
     setTimeout(function () {
         spans[i].classList.add('animate-in');
+        // Trigger image animation after the last span of h2
+        if (i === spans.length - 1) {
+            setTimeout(function () {
+                image.classList.add('animate-in');
+            }, var(--duration));
+        }
     }, 120 * i);
 }
 
-function animateImageAndButton() {
-    setTimeout(function () {
-        image.classList.add('animate-in');
-    }, 120 * spans.length);
-
+function animateButton() {
     setTimeout(function () {
         button.classList.add('animate-in');
-    }, 120 * spans.length + 550); // 550ms is the duration of the animation
+    }, 120 * spans.length + var(--duration) * 2); // Wait for image animation to complete
 }
 
 window.addEventListener('load', function () {
     for (var i = 0; i < spans.length; i++) {
         doSetTimeout(i);
     }
-    animateImageAndButton();
+    animateButton();
 });
